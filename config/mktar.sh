cbd90c93d2f10ce1df8fb1afd1d49633d0ce00e0
Gustaf Neumann <neumann@wu-wien.ac.at>
2008-02-22 15:20:13 -0600

    removing test file

--- config/mktar.sh
+++ config/mktar.sh
@@ -7,7 +7,7 @@ echo "name=$name"
 make distclean
 cd ..
 tar zcvf ./$name.tar.gz \
-    `find ./$name -type f -o -type l| fgrep -v CVS | fgrep -v SCCS | \
+    `find ./$name -type f -o -type l| fgrep -v .git| fgrep -v CVS | fgrep -v SCCS | \
 	fgrep -v Attic | fgrep -v "autom4te"| fgrep -v "~"|fgrep -v .db | \
 	fgrep -v .junk | fgrep -v "#" |fgrep -v .DS_Store| fgrep -v config. | \
         fgrep -v .gdb`
