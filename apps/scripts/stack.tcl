5c200556d0edbe7ef71130caf11c6d04fb0acf0e
Gustaf A. Neumann <neumann@Gustaf-A-Neumanns-iMac.local>
2010-08-02 10:56:25 +0200

    - added minimal stack example

---
+++ apps/scripts/stack.tcl
@@ -0,0 +1,21 @@
+package req next
+
+nx::Class create stack {
+  :method init {} {set :things {}}
+  :public method push {thing} {
+    set :things [linsert ${:things} 0 $thing]
+    return $thing
+  }
+  :public method pop {} {
+    set top [lindex ${:things} 0]
+    set :things [lrange ${:things} 1 end]
+    return $top
+  }
+}
+
+stack create s1
+puts [s1 push 1]
+puts [s1 push 2]
+puts [s1 pop]
+puts [s1 pop]
+puts [s1 pop]
