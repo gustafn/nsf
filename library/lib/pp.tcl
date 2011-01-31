57570354bfebc1bc24f1ba3d7976c44b2c2bd3e9
Gustaf A. Neumann <neumann@Gustaf-A-Neumanns-iMac.local>
2011-01-31 12:53:36 +0100

    - updated documentation

--- library/lib/pp.tcl
+++ library/lib/pp.tcl
@@ -96,7 +96,7 @@ namespace eval ::nx::pp {
       tell trace unset uplevel update upvar variable vwait while
       package
       public protected class-object
-      method attribute forward 
+      method alias attribute forward 
       my next new self current
       create init new destroy alloc dealloc
       class superclass mixin filter
