02c5fd2264c925e0e8501baeb0e33a42ea0ff992
Gustaf Neumann <neumann@wu-wien.ac.at>
2011-10-25 13:54:21 +0200

    - added copyright notice to nx.tc and xotcl2.tcl

--- library/xotcl/library/xotcl2.tcl
+++ library/xotcl/library/xotcl2.tcl
@@ -1,3 +1,33 @@
+############################################################
+# xotcl2.tcl - 
+#
+#      Implementation of the XOTcl 2 object systen, based
+#      on the Next Scripting Framework
+#
+# Copyright (C) 2010-2011 Gustaf Neumann
+# Copyright (C) 2010-2011 Stefan Sobernig
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
 package provide XOTcl 2.0b2
 package require nx
 
