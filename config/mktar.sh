69cf721ea2eb81c0df8c2b4bfb831c367780a59b
Gustaf Neumann <neumann@wu-wien.ac.at>
2011-10-26 23:44:16 +0200

    - updated build scripts to reflect dropped files and to reduce xotcl-references

--- config/mktar.sh
+++ config/mktar.sh
@@ -9,6 +9,7 @@ cd ..
 tar zcvf ./$name.tar.gz \
     `find ./$name -type f -o -type l| fgrep -v .git| fgrep -v .dSYM | fgrep -v .DS_Store| \
     	fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk | \
+    	fgrep -v "doc/example*.txt"| \
     	fgrep -v ".dylib"| \
 	fgrep -v "autom4te"| fgrep -v config. | fgrep -v callgrind.out. | fgrep -v .plist |\
         fgrep -v .db | fgrep -v .gdb | fgrep -v .graffle`
