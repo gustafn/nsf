c7f47955cd341cc54e37ae6621da56a0ca0d0f73
Gustaf Neumann <neumann@wu-wien.ac.at>
2012-02-01 11:33:11 +0100

    extended blacklist for tar file

--- config/mktar.sh
+++ config/mktar.sh
@@ -8,7 +8,8 @@ make distclean
 cd ..
 tar zcvf ./$name.tar.gz \
     `find ./$name -type f -o -type l| fgrep -v .git| fgrep -v .dSYM | fgrep -v .DS_Store| \
-    	fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk | \
+    	fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk |\
+	fgrep -v .gcov |fgrep -v .gcda|fgrep -v .gcno|fgrep -v lcov-result|fgrep -v lcov.info|\
     	egrep -v "doc/example.*[.]txt"| \
     	fgrep -v ".dylib"| \
 	fgrep -v "autom4te"| fgrep -v config. | fgrep -v callgrind.out. | fgrep -v .plist |\
