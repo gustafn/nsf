2154d899afddb7acd71ceb258af0004fcb919ba6
Gustaf Neumann <neumann@wu-wien.ac.at>
2013-06-10 11:14:52 +0200

    - more release work

--- config/mktar.sh
+++ config/mktar.sh
@@ -8,9 +8,9 @@ make distclean
 cd ..
 tar zcvf ./$name.tar.gz \
     `find ./$name -type f -o -type l| fgrep -v .git| fgrep -v .dSYM | fgrep -v .DS_Store| \
-    	fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk |\
+    	fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk |  egrep -v 'generic.*[.]c-' |\
 	fgrep -v .gcov |fgrep -v .gcda|fgrep -v .gcno|fgrep -v lcov-result|fgrep -v lcov.info|\
-    	egrep -v "doc/example.*[.]txt"| \
+    	egrep -v "doc/example.*[.]txt"| fgrep -v "doc/prince.js" | egrep -v 'doc/.*[.]pdf' |\
     	fgrep -v ".dylib"| \
 	fgrep -v "autom4te"| fgrep -v config. | fgrep -v callgrind.out. | fgrep -v .plist |\
         fgrep -v .db | fgrep -v .gdb | fgrep -v .graffle`
