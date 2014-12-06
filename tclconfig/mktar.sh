9fe4a51744339ba878c9a276268f720206e49d5b
Gustaf Neumann <neumann@wu-wien.ac.at>
2014-12-06 16:20:34 +0100

    - make sure to omit Mac OS X hidden file from tar balls

--- tclconfig/mktar.sh
+++ tclconfig/mktar.sh
@@ -11,7 +11,7 @@ tar zcvf ./$name.tar.gz \
         fgrep -v "~"| fgrep -v "#" | fgrep -v -- -orig | fgrep -v .junk |\
         fgrep -v .gcov |fgrep -v .gcda|fgrep -v .gcno|fgrep -v lcov-result|fgrep -v lcov.info|\
         egrep -v "doc/example.*[.]txt"| \
-        fgrep -v ".dylib"| \
+        fgrep -v .dylib | fgrep -v ._ | egrep -v ".o$" | \
         fgrep -v "autom4te"| fgrep -v config. | fgrep -v callgrind.out. | fgrep -v .plist |\
         fgrep -v .db | fgrep -v .gdb | fgrep -v .graffle`
 
