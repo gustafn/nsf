3ee0ac91f0a8034ec4739bc2af1016cdf30fbe57
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-04-29 16:15:27 +0200

    Some cleanup and adding basic test cases

--- library/lib/nx-shell.tcl
+++ library/lib/nx-shell.tcl
@@ -16,15 +16,7 @@ nx::Object create ::nx::shell2 {
       return
     }
     if {[info complete ${:line}]} {
-      set script [list catch [string trim ${:line}] [current]::result [current]::opts]
-      set r [uplevel #0 $script]
-      if {$r} {
-        puts stdout [dict get ${:opts} -errorinfo]
-        unset :opts;
-      } else {
-        puts stdout ${:result}
-        unset :result
-      }
+      :evalScript ${:line}
       unset :line
       if {![info exists :forever]} {
         :prompt
@@ -32,23 +24,72 @@ nx::Object create ::nx::shell2 {
     }
   }
 
-  :public object method prompt {{chan stdout}} {
+  :protected object method evalScript {-exit:switch script} {
+    set script [list catch [string trim $script] [current]::result [current]::opts]
+    set r [uplevel #0 $script]
+    if {$r} {
+      puts stderr [dict get ${:opts} -errorinfo]
+      if {$exit} {
+        exit 1
+      } else {
+        unset :opts;
+      }
+    } else {
+      if {${:result} ne ""} {
+        puts stdout ${:result}
+      }
+      unset :result
+    }
+  }
+
+  :protected object method prompt {{chan stdout}} {
     puts -nonewline $chan "% "
     flush $chan
   }
 
   :public object method run {argc argv} {
-    if {$argc == 0} {
-      :prompt stdout
-      fconfigure stdin -blocking 0 -buffering line
-      fileevent stdin readable [list [current] onRead]
-      vwait :forever
-      fileevent stdin readable {}
-      exit
+    if {[catch {
+      if {$argc == 0} {
+        # interactive mode
+        :prompt stdout
+        fconfigure stdin -blocking 0 -buffering line
+        fileevent stdin readable [list [current] onRead]
+        vwait :forever
+        fileevent stdin readable {}
+        exit
+      } else {
+        # non-interactive modes
+        :nonInteract {*}$argv
+      }
+    } msg]} {
+      puts stderr "[current] failed unexpectedly with '$msg'"
+      exit 2
+    }
+  }
+
+  :protected object method nonInteract {-c:switch args} {
+    if {$c} {
+      # non-interactive mode: arg command xor stdin
+      if {[llength $args]} {
+        # arg command plus argc/argv
+        set args [lassign $args script]
+        set ::argv $args
+        set ::argc [llength $args]
+      } else {
+        # stdin
+        set ::argv ""
+        set ::argc 0
+        set script [gets stdin]
+      }
+      :evalScript -exit $script
     } else {
-      set ::argv [lassign $argv argv0]
+      # non-interactive mode: script
+      set ::argv [lassign $args argv0]
       incr ::argc -1
-      uplevel #0 [list source $argv0]
+      if {[catch {uplevel #0 [list source $argv0]} msg]} {
+        puts stderr $msg
+        exit 1
+      }
     }
   }
 }
