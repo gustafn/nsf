c00fbde4c8915bcb44734039601af5cbeaab2971
Gustaf Neumann <neumann@wu-wien.ac.at>
2016-12-05 12:33:52 +0100

    - white space change (avoid tabs)

--- generic/nsf.c
+++ generic/nsf.c
@@ -18829,7 +18829,7 @@ static void
 TclDeletesObject(ClientData clientData) {
   NsfObject *object;
   Tcl_Interp *interp;
-  
+
   nonnull_assert(clientData != NULL);
 
   object = (NsfObject *)clientData;
@@ -23078,11 +23078,11 @@ ListMethod(Tcl_Interp *interp,
       {
         Proc *procPtr = GetTclProcFromCommand(cmd);
 
-	if (procPtr == NULL) {
+        if (procPtr == NULL) {
           Tcl_SetObjResult(interp, Tcl_NewStringObj("body not available for this kind of method", -1));
           return TCL_ERROR;
-	}
-	if (procPtr->bodyPtr->typePtr == Nsf_OT_byteCodeType) {
+        }
+        if (procPtr->bodyPtr->typePtr == Nsf_OT_byteCodeType) {
           EXTERN Tcl_Obj *Tcl_DisassembleByteCodeObj(Tcl_Interp *interp, Tcl_Obj *objPtr);
 
           Tcl_SetObjResult(interp, Tcl_DisassembleByteCodeObj(interp, procPtr->bodyPtr));
