89f7f5d879f2881445c70e1c5b4be391cabaf6a0
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2016-12-22 12:37:41 +0100

    * nx-shell.tcl, *sh.in: Rename nx::shell2 to nx::shell and remove old   nx::shell implemenetation. * makefile.vc: Use new shell object in shell target.

--- library/lib/nx-shell.tcl
+++ library/lib/nx-shell.tcl
@@ -42,7 +42,7 @@ package require nx
 
 package provide nx::shell 1.1
 
-nx::Object create ::nx::shell2 {
+nx::Object create ::nx::shell {
 
   :public object method onRead {{chan stdin}} {
     append :line [read $chan]
@@ -218,42 +218,6 @@ nx::Object create ::nx::shell2 {
         interp alias {} ::try {}
         interp expose {} try;
       }
-    }
-  } 
-  
-}
-
-nx::Object create ::nx::shell {
-  :public object method run {argc argv} {
-    if {$argc == 0} {
-      set prefix ""
-      set line ""
-      while {1} {
-        update
-        if {$line eq ""} {
-          puts -nonewline "% "
-          flush stdout
-        }
-        append line [gets stdin]
-        if {[info complete $line]} {
-          set script [list catch $line [current]::result [current]::opts]
-          set r [uplevel #0 $script]
-          if {$r} {
-            puts [dict get ${:opts} -errorinfo]
-            unset :opts;
-          } else {
-            puts ${:result}
-            unset :result
-          }          
-          set line ""
-          continue
-        }
-        append line \n 
-      }
-    } else {
-      set ::argv [lassign $argv argv0]
-      incr ::argc -1
-      uplevel #0 [list source $argv0]
     }
   }
 }
