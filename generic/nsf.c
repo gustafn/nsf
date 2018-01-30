42350dcac26a773a14673da7ece98810ba3fd052
Stefan Sobernig <stefan.sobernig@wu.ac.at>
2018-01-30 14:54:21 +0100

    * nsf.c (NextSearchAndInvoke): Relax the pre-conditions, objv can   actually be NULL (see test cases). Besides, the assertion was not   reflected by a corresponding nonnull constraints on the   NextSearchAndInvoke prototype.

--- generic/nsf.c
+++ generic/nsf.c
@@ -19025,7 +19025,7 @@ NextSearchAndInvoke(Tcl_Interp *interp, const char *methodName,
 
   nonnull_assert(interp != NULL);
   nonnull_assert(methodName != NULL);
-  nonnull_assert(objv != NULL);
+  /* nonnull_assert(objv != NULL); */
   nonnull_assert(cscPtr != NULL);
 
   /*
