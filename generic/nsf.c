3e6e82e95cb7d22117f884cdddf8a32d77ec930d
Gustaf Neumann <neumann@wu-wien.ac.at>
2024-09-27 13:25:04 +0200

    Improved Tcl 9.0.0 compatibility
    One needs 'Tcl_InitStubs(interp, "8.5-",...)' instead of
    'Tcl_InitStubs(interp, "8.5",...)', since the semantics are seemingly
    the same as in "package require ...". Not sure, why we have not seen
    this with earlier pre-releases of Tcl9.
    
    Without this change, we see in the NaviServer regression test the following error message:
    
    [27/Sep/2024:10:56:30][18309.7f35ed856540][-main:test-] Error: version conflict for package "tcl": have 9.0.0, need 8.5
    :    version conflict for package "tcl": have 9.0.0, need 8.5
    :        while executing
    :    "load {} Nsf"
    :        invoked from within
    :    "ns_ictl update"
    :        (procedure "ns_cleanup" line 8)
    :        invoked from within

--- generic/nsf.c
+++ generic/nsf.c
@@ -36173,13 +36173,13 @@ Nsf_Init(
    */
 
   if (stubsInitialized == 0) {
-    if (Tcl_InitStubs(interp, "8.5", 0) == NULL) {
+    if (Tcl_InitStubs(interp, "8.5-", 0) == NULL) {
       return TCL_ERROR;
     }
 # if TCL_MAJOR_VERSION > 8 || TCL_MINOR_VERSION > 6
     /* Tcl_TomMath_InitStubs() not needed */
 # else
-    if (Tcl_TomMath_InitStubs(interp, "8.5") == NULL) {
+    if (Tcl_TomMath_InitStubs(interp, "8.5-") == NULL) {
       return TCL_ERROR;
     }
 # endif
