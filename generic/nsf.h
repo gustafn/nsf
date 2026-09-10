41132f647ddfcdde2fb0c5d67d92f2b5a238102a
Gustaf Neumann <neumann@wu-wien.ac.at>
2026-09-10 19:15:08 +0200

    fix: add leading semicolon to NS_FALL_THROUGH macro - Prefix fallthrough attribute with `;` for clang and GCC >= 7 - The leading semicolon seems necessary for native GCC

--- generic/nsf.h
+++ generic/nsf.h
@@ -513,16 +513,15 @@ typedef struct Nsf_Param {
 
 #if defined(__clang__)
 # if __has_attribute(fallthrough)
-#  define NSF_FALL_THROUGH __attribute__((fallthrough))
+#  define NS_FALL_THROUGH ; __attribute__((fallthrough))
 # else
-#  define NSF_FALL_THROUGH ((void)0)
+#  define NS_FALL_THROUGH ((void)0)
 # endif
 #elif __GNUC_PREREQ(7, 0)
-# define NSF_FALL_THROUGH __attribute__((fallthrough))
+# define NS_FALL_THROUGH ; __attribute__((fallthrough))
 #else
-# define NSF_FALL_THROUGH ((void)0)
+# define NS_FALL_THROUGH ((void)0)
 #endif
-
 
 /*
 EXTERN int
